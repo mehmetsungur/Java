0f30d3ba157ff9c811a4370ad1deb506c18edaa7
Mehmet Sungur <mehmetsungur90@gmail.com>
2023-06-05 13:37:17 +0300

    update

---
+++ Java/src/main/java/day20Constructor/TeacherRunner.java
@@ -0,0 +1,10 @@
+package day20Constructor;
+public class TeacherRunner {
+    public static void main(String[] args) {
+        Teacher t1 = new Teacher("Ali Can",40,"Istanbul");
+        Teacher t2 = new Teacher("Haydar",32);
+
+        System.out.println(t1);
+        System.out.println(t2);
+    }
+}
