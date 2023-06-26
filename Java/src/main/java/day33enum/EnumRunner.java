8ba4aa9f2f278ffd2e4952fa7d5f4f32385ae353
Mehmet Sungur <mehmetsungur90@gmail.com>
2023-06-26 12:28:14 +0300

    update

---
+++ Java/src/main/java/day33enum/EnumRunner.java
@@ -0,0 +1,9 @@
+package day33enum;
+
+public class EnumRunner {
+    public static void main(String[] args) {
+        //Enum daki herhangi bir sabit değere ulaşma
+        Cities hatay = Cities.HATAY;
+        System.out.println(hatay);
+    }
+}
