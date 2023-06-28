c1afb6664ed336ce90dd7a320bc8dc8703dec31a
Mehmet Sungur <mehmetsungur90@gmail.com>
2023-06-28 13:56:26 +0300

    update

--- Java/src/main/java/oldjava/TypeCasting.java
+++ Java/src/main/java/oldjava/TypeCasting.java
@@ -4,6 +4,17 @@ import java.util.Scanner;
 
 public class TypeCasting {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Day: ");
+        String day = scan.next();
 
+        if(day.equalsIgnoreCase("Cuma"))
+            System.out.println("Muslim");
+        else if (day.equalsIgnoreCase("Cumartesi"))
+            System.out.println("Indian");
+        else if (day.equalsIgnoreCase("Pazar"))
+            System.out.println("Christian");
+        else
+            System.out.println("Invalid Day");
     }
 }
