6509d6d825d113098ddae8a1fa31928c423581d2
mehmetsungur <mehmetsungur90@gmail.com>
2023-07-08 14:14:07 +0300

    update

--- Java/src/main/java/day36lambda/Lambda02.java
+++ Java/src/main/java/day36lambda/Lambda02.java
@@ -3,6 +3,9 @@ package day36lambda;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.*;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Lambda02 {
     public static void main(String[] args) throws IOException {
@@ -24,5 +27,32 @@ public class Lambda02 {
                 .lines(Paths.get("Java/src/main/java/day36lambda/LambdaTextFile.txt"))
                 .anyMatch(t->t.contains("get"));
         System.out.println(result);
+
+        //Example 4: Verilen text file icindeki text'lerde kullanılan farklı kelimeleri bir list içnde return ediniz.
+        List<String> words = Files
+                .lines(Paths.get("Java/src/main/java/day36lambda/LambdaTextFile.txt"))
+                .map(t->t.replaceAll("\\p{Punct}","")
+                        .split(" "))
+                .flatMap(Arrays::stream)
+                .distinct().collect(Collectors.toList());
+        System.out.println(words);
+
+        //Example 5: Verilen text file icindeki text'lerde toplam kaç harf kullanıldığını bulunuz.
+        long count = Files
+                .lines(Paths.get("Java/src/main/java/day36lambda/LambdaTextFile.txt"))
+                .map(t->t.replaceAll("^a-zA-Z","")
+                        .split(""))
+                .flatMap(Arrays::stream)
+                .count();
+        System.out.println(count);
+
+        //Example 6: Verilen text file icindeki text'de kullanilan tum harfleri alfabetik olarak ters sirada bir listin icinde return eden kodu yaziniz.
+        List<String> letters = Files.lines(Paths.get("Java/src/main/java/day36lambda/LambdaTextFile.txt"))
+                .map(t -> t.replaceAll("[^a-zA-Z]", ""))
+                .map(t -> t.split(""))
+                .flatMap(Arrays::stream)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+        System.out.println(letters);
     }
 }
