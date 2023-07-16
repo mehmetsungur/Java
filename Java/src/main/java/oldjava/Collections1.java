6dbf536f66e71758822606bb0dee668b77b6bec7
mehmetsungur <mehmetsungur90@gmail.com>
2023-07-16 22:04:54 +0300

    update

---
+++ Java/src/main/java/oldjava/Collections1.java
@@ -0,0 +1,28 @@
+package oldjava;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Collections1 {
+    public static void main(String[] args) {
+        LinkedList<String> ll = new LinkedList<>();
+        ll.add("A");
+        ll.add("B");
+        ll.add("C");
+        ll.add("D");
+        System.out.println(ll);
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Name: ");
+        String isim = scan.nextLine();
+
+        if(ll.remove(isim)){
+            System.out.println("Silindi");
+            System.out.println(ll);
+        }
+        else{
+            System.out.println("Ne Hırsızı");
+            System.out.println(ll);
+        }
+    }
+}
