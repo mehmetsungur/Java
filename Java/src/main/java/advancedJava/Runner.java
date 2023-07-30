fce6eefa3aad2f38f22d37b220d99c42c92d9b15
mehmetsungur <mehmetsungur90@gmail.com>
2023-07-30 23:21:54 +0300

    update

--- Java/src/main/java/advancedJava/Runner.java
+++ Java/src/main/java/advancedJava/Runner.java
@@ -13,5 +13,13 @@ public class Runner {
 
         int num = (int) obj01.getField();
         System.out.println(num);
+
+        Advanced02<String> obj03 = new Advanced02<>();
+        obj03.setField("Generic Types");
+        String str1 = obj03.getField();
+
+        Advanced02<Integer> obj04 = new Advanced02<>();
+        obj04.setField(646);
+        Integer num1 = obj04.getField();
     }
 }
